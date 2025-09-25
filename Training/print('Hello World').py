e682b7dc738ae52acf17558493576e94cbb0e207
Yoseph Tigiste <26tigi@gmail.com>
2025-09-25 21:09:51 +0300

    django_girls cont.

--- Training/print('Hello World').py
+++ Training/print('Hello World').py
@@ -1,1 +1,18 @@
 print('Hello World')
+list = [6, 2, 4, 2, 2]
+for num in list:
+    print('$' * num)
+if 3 > 2:
+    print('It works!')
+def hi():
+    print('Hi there!')
+    print('How are you?')
+hi()
+def hi(name):
+    print(f'Hi {name}!')
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+for name in girls:
+    hi (name)
+    print('Next girl')
+for i in range(1, 6):
+    print(i)
